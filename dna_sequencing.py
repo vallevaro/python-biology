f6ce35111fd399cee33b6738b462d64a9f4ea7b6
vallevaro <mvallevaro@gmail.com>
2023-01-16 11:30:07 +0100

    Python script that can be used to analyze a DNA sequence and create some important plots to understand its structure.

---
+++ dna_sequencing.py
@@ -0,0 +1,42 @@
+# Import necessary libraries
+import matplotlib.pyplot as plt
+from Bio import SeqIO
+
+# Load the DNA sequence from a FASTA file
+record = SeqIO.read("dna_sequence.fasta", "fasta")
+sequence = record.seq
+
+# Compute GC content
+gc_content = (sequence.count("G") + sequence.count("C")) / len(sequence)
+
+# Create a GC skew plot
+gc_skew = []
+window = 100
+for i in range(0, len(sequence) - window, window):
+    gc_count = (sequence[i:i+window].count("G") + sequence[i:i+window].count("C"))
+    at_count = (sequence[i:i+window].count("A") + sequence[i:i+window].count("T"))
+    gc_skew.append((gc_count - at_count) / (gc_count + at_count))
+
+plt.plot(gc_skew)
+plt.xlabel("Window Position (bp)")
+plt.ylabel("GC Skew")
+plt.title("GC Skew Plot")
+plt.show()
+
+# Create a codon usage plot
+codon_counts = {}
+for i in range(0, len(sequence), 3):
+    codon = sequence[i:i+3]
+    if codon in codon_counts:
+        codon_counts[codon] += 1
+    else:
+        codon_counts[codon] = 1
+
+plt.bar(codon_counts.keys(), codon_counts.values())
+plt.xlabel("Codon")
+plt.ylabel("Count")
+plt.title("Codon Usage Plot")
+plt.show()
+
+# Print GC content
+print("GC Content:", gc_content)
